dd33532747a310912099db4fab6cec15d6a1ca06
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-06-22 21:05:26 +0300

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.scss";
+import Nav from "@/components/nav/Nav";
+import Footer from "@/components/footer/Footer";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Simple CRUD app",
+  description: "NextJS practice",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <div id="root">
+          <Nav />
+          {children}
+
+          <Footer />
+        </div>
+      </body>
+    </html>
+  );
+}
