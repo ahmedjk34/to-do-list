fc77d51968c4731821cb77ca4aec3d4100b34ccf
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-06-23 14:35:42 +0300

    fixed imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.scss";
-import Nav from "@/components/nav/Nav";
 import Footer from "@/components/footer/Footer";
+import Nav from "@/components/nav/Nav";
 
 const inter = Inter({ subsets: ["latin"] });
 
