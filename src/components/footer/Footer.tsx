eaa79aefba68b374b861a97d77322e76a34d9100
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-06-23 17:25:17 +0300

    footer and it's styling

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import styles from "./footer.module.scss";
+import { FaGithub } from "react-icons/fa";
 
 type Props = {};
 
 function Footer({}: Props) {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.footer}>
+      <h3>A Simple CRUD App with NextJS & MongoDB</h3>
+      <h3>Made By : {<FaGithub />} Ahmedjk34</h3>
+    </div>
+  );
 }
 
 export default Footer;
